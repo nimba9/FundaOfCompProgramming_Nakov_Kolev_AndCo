a7650ed5ceec21d72c3f6ea68d11f153a19a15d1
vancisin peter <vancisin.peter@gmail.com>
2017-11-01 19:12:01 +0100

    Chapter 16, exercise 05

---
+++ sln_Chp16_RemoveNegIntFromList/nakovChp16_RemoveNegIntFromList/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    public static void RemoveNegatINT(List<int> numList)
+    {
+
+        if (numList.Count == 0)
+        {
+            throw new NullReferenceException("No data found in the list");
+        }
+
+
+        List<int> result = new List<int>();
+
+
+
+        foreach (var item in numList)
+        {
+            if (item > 0)
+            {
+                result.Add(item);
+            }
+        }
+
+        numList = result;
+
+        foreach (var item in result)
+        {
+            Console.WriteLine(item);
+        }
+
+
+    }
+
+
+
+    public static void Main()
+    {
+        List<int> intList = new List<int>() { 2, 4, -6, -6, -34, -56, 67, 67, 67, -82, -83, -83 };
+
+        RemoveNegatINT(intList);
+    }
+}
+
