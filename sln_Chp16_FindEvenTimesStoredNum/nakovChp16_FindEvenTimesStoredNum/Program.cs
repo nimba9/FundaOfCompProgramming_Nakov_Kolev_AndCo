19ace267da3a6d26da6be25903d4f193a2815a75
vancisin peter <vancisin.peter@gmail.com>
2017-11-08 23:04:14 +0100

    update

--- sln_Chp16_FindEvenTimesStoredNum/nakovChp16_FindEvenTimesStoredNum/Program.cs
+++ sln_Chp16_FindEvenTimesStoredNum/nakovChp16_FindEvenTimesStoredNum/Program.cs
@@ -13,21 +13,25 @@ namespace nakovChp16_FindEvenTimesStoredNum
             List<int> evenList = new List<int>();
 
             int[] arr = seque.ToArray();
-            //Array.Sort(arr);
+            Array.Sort(arr);
+
+            
 
             int timesOccured = 1;
 
-            for (int i = 0; i < arr.Length - 1; i++)
+            for (int i = 0; i < arr.Length -1; i++)
             {
                 if (arr[i] == arr[i + 1])
                 {
                     timesOccured++;
+                    //evenList.Add(arr[i]);
+                 
                 }
 
 
                 else if ((timesOccured % 2) == 0)
                 {
-                    evenList.Add(arr[1]);
+                    evenList.Add(arr[i]);
                     timesOccured = 1;
                 }
 
@@ -41,7 +45,7 @@ namespace nakovChp16_FindEvenTimesStoredNum
 
         static void Main(string[] args)
         {
-            List<int> check = new List<int>() { 5, 5, 12, 14, 26, 32, 26 };
+            List<int> check = new List<int>() { 77, 5, 12, 14, 26, 32, 26, 5, 77 };
 
             SortEvenOccurence(check);
         }
