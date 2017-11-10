2b20e935cbab9905b0f7b45842e4b118ea7d06e3
vancisin peter <vancisin.peter@gmail.com>
2017-11-10 19:25:20 +0100

    1st version

---
+++ sln_Chp16_countOccurInt/nakovChp16_countOccurInt/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace nakovChp16_countOccurInt
+{
+    class Program
+    {
+        public static void CountOccurInt(int [] arr)
+        {
+            Dictionary<int, int> intCount= new Dictionary<int, int>();
+            
+            foreach (var item in arr)
+            {
+                if (intCount.ContainsKey(item))
+                {
+                    intCount[item]++;
+                    
+                }
+
+                else
+                {
+                    intCount.Add(item, 1);
+                }
+            }
+
+            Console.WriteLine("The numbers in sequence occured as followed:");
+            foreach (var item in intCount)
+            {
+                Console.Write("\nNumber: " + item.Key + "; Occurences: " + item.Value);
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            
+           int[] testArr = { 3, 4, 4, 2, 3, 3, 4, 3, 2 };
+
+           CountOccurInt(testArr);
+
+        }
+    }
+}
---
+++ sln_sandbox/sandbox/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace sandbox
+{
+    class Program
+    {
+        /*
+         * Program for finding same numbers in sequence that occur even times.
+         */
+         
+    }
+}
