ed94f7ebba7dcb566a7050be8edfc850f18bc282
vancisin peter <vancisin.peter@gmail.com>
2017-11-12 22:22:13 +0100

    update

--- sln_Chp16_FindOddTimesStoredNum/nakovChp16_FindOddTimesStoredNum/Program.cs
+++ sln_Chp16_FindOddTimesStoredNum/nakovChp16_FindOddTimesStoredNum/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace nakovChp16_FindOddTimesStoredNum
+namespace nakovChp16_EvenTimeStoredNum_LoopVersion
 {
     class Program
     {
