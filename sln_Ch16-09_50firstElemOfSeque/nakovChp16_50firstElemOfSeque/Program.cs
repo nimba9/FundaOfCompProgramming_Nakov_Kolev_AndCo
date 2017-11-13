17a78dc8bb022d9e4c7a07833d93826c6b183511
vancisin peter <vancisin.peter@gmail.com>
2017-11-13 21:02:09 +0100

    1st version

---
+++ sln_Ch16-09_50firstElemOfSeque/nakovChp16_50firstElemOfSeque/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace nakovChp16_50firstElemOfSeque
+{
+    class Program
+    {
+
+        public static void PrintRecurrSeque(int num)
+        {
+            Queue<int> seque = new Queue<int>();
+
+            seque.Enqueue(num);
+
+            for (int i = 0; i < 50; i++)
+            {
+                int currNum = seque.Dequeue();
+                Console.WriteLine(currNum);
+                seque.Enqueue(currNum + 1);
+                seque.Enqueue(2 * currNum + 1);
+                seque.Enqueue(currNum + 2);
+            }
+            
+            
+        }
+
+
+        static void Main(string[] args)
+        {
+            PrintRecurrSeque(2);
+        }
+    }
+}
