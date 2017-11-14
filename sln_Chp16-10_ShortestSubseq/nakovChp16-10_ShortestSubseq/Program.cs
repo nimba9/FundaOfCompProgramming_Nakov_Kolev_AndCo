5877ed111cb53464681887c738392754684d6ee0
vancisin peter <vancisin.peter@gmail.com>
2017-11-14 23:29:08 +0100

    1st version

---
+++ sln_Chp16-10_ShortestSubseq/nakovChp16-10_ShortestSubseq/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace ShortestSequence
+{
+    
+
+    public class ShortestSequence
+    {
+        static int N = 5;
+        static int M = 16;
+
+        class Node<T>
+        {
+            public T Value
+            {
+                get;
+                set;
+            }
+
+            public Node<T> Previous
+            {
+                get;
+                set;
+            }
+        }
+
+        private static void CreateNewNode(Node<int> currentNode, int newValue, Queue<Node<int>> queue)
+        {
+            var nodeOne = new Node<int>();
+            nodeOne.Value = newValue;
+            nodeOne.Previous = currentNode;
+            if (nodeOne.Value == M)
+            {
+                Print(nodeOne);
+                queue.Clear();
+            }
+            else if (nodeOne.Value < M)
+            {
+                queue.Enqueue(nodeOne);
+            }
+        }
+
+        private static void Print(Node<int> nodeOne)
+        {
+            var result = new Stack<int>();
+            var currentNode = nodeOne;
+            while (currentNode.Previous != null)
+            {
+                result.Push(currentNode.Value);
+                currentNode = currentNode.Previous;
+            }
+
+            result.Push(currentNode.Value);
+            var sb = new StringBuilder();
+            while (result.Count > 0)
+            {
+                if (sb.Length == 0)
+                {
+                    sb.Append(result.Pop());
+                }
+                else
+                {
+                    sb.Append(" - >" + result.Pop());
+                }
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+
+        private static int OperationOne(int number)
+        {
+            return number += 1;
+        }
+
+        private static int OperationTwo(int number)
+        {
+            return number += 2;
+        }
+
+        private static int OperationMultiply(int number)
+        {
+            return number *= 2;
+        }
+
+        public static void Main()
+        {
+            var start = new Node<int>();
+            start.Value = N;
+            var queue = new Queue<Node<int>>();
+            queue.Enqueue(start);
+            while (queue.Count > 0)
+            {
+                var currentNode = queue.Dequeue();
+                CreateNewNode(currentNode, OperationOne(currentNode.Value), queue);
+                if (queue.Count > 0)
+                {
+                    CreateNewNode(currentNode, OperationTwo(currentNode.Value), queue);
+                }
+
+                if (queue.Count > 0)
+                {
+                    CreateNewNode(currentNode, OperationMultiply(currentNode.Value), queue);
+                }
+            }
+        }
+    }
+ 
+}
