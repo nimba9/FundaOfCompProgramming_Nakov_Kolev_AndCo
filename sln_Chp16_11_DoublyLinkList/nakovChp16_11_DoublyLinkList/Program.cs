88e10a0c5bf69d2fa203df3b0c1335356da95eb7
vancisin peter <vancisin.peter@gmail.com>
2017-11-20 23:16:10 +0100

    example update

--- sln_Chp16_11_DoublyLinkList/nakovChp16_11_DoublyLinkList/Program.cs
+++ sln_Chp16_11_DoublyLinkList/nakovChp16_11_DoublyLinkList/Program.cs
@@ -32,7 +32,7 @@ public class LinkedListCustom<T> : IEnumerable<T>
         this.AddFirst(newItem);
     }
 
-    public void AddLast(LinkedListCustom<T> item)
+    public void AddLast(ListItemCustom<T> item)
     {
         if (this.Count == 0)
         {
@@ -97,11 +97,81 @@ public class LinkedListCustom<T> : IEnumerable<T>
 
     public void AddAfter(ListItemCustom<T> node, ListItemCustom<T> newItem)
     {
+        if (node == null || newItem == null)
+        {
+            throw new ArgumentNullException();
+        }
 
+        if (node.NextItem == null)
+        {
+            this.AddLast(newItem);
+            return;
+        }
+
+        newItem.NextItem = node.NextItem;
+        node.NextItem = newItem;
+        this.Count++;
     }
 
+    public void AddAfter(ListItemCustom<T> node, T newItem)
+    {
+        ListItemCustom<T> newNode = new ListItemCustom<T>(newItem);
+        this.AddAfter(node, newNode);
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        var currentNode = this.FirstItem;
+
+        while (currentNode != null)
+        {
+            yield return currentNode.Value;
+            currentNode = currentNode.NextItem;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
        
 }
+
+public class ListItemCustom<T>
+{
+    public ListItemCustom(T value)
+    {
+        this.Value = value;
+    }
+
+    public T Value { get; set; }
+
+    public ListItemCustom<T> NextItem { get; set; }
+}
+
+public class TestProgram
+{
+    public static void Main()
+    {
+        LinkedListCustom<int> list = new LinkedListCustom<int>();
+
+        for (int i = 1; i <= 10; i++)
+        {
+            list.AddLast(i);
+        }
+    }
+
+    private static void PrintList(LinkedListCustom<int> list)
+    {
+        foreach (var node in list)
+        {
+            Console.WriteLine(node + " ");
+        }
+
+        //Console.WriteLine();
+    }
+}
+
 
 
 
